43c50e91e1ddee762311911db2d159ecd6bbc4ee
Ivona Pepic <ivona.pepic@gmail.com>
2022-09-23 15:51:46 +0200

    created nested routes for shop

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,33 @@
+import { createContext, useState, useEffect } from "react";
+
+// import SHOP_DATA from "../shop-data.js";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+
+  //to add the data to our firebase db (comment it out, so that it doesnt run everytime)
+
+  // useEffect(() => {
+  //     addCollectionAndDocuments("categories", SHOP_DATA);
+  // }, []);
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
